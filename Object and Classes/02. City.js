bcdf4b9d4701aadf663d32f4d5dc921090c7908b
Stefan Dimitrov <54206891+StefanDimitrovDimitrov@users.noreply.github.com>
2021-01-16 17:54:42 +0200

    Initial Commit

---
+++ Object and Classes/02. City.js
@@ -0,0 +1,17 @@
+function city(obj){
+for (const key of Object.keys(obj)) {
+    
+    console.log(`${key} -> ${obj[key]}`)
+}
+    
+}
+
+
+city({
+    name: "Sofia",
+    area: 492,
+    population: 1238438,
+    country: "Bulgaria",
+    postCode: "1000"
+}
+)
