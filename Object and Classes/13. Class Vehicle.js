b080001805efde5039f971841d2ebe7f2061c968
Stefan Dimitrov <54206891+StefanDimitrovDimitrov@users.noreply.github.com>
2021-01-19 16:04:23 +0200

    Initial Commit

--- Object and Classes/13. Class Vehicle.js
+++ Object and Classes/13. Class Vehicle.js
@@ -1,23 +1,20 @@
-// function solve(){
 
-    class Vehicle{
-        constructor(type,model,parts,fuel){
-            this.type = type
-            this.model = model
-            this.parts = parts
-            this.parts.quality = parts.engine * parts.power
-            this.fuel = fuel
-
-        }
-
-        drive(fuelLose){
-            this.fuel -= fuelLose
-        }
+class Vehicle{
+    constructor(type,model,parts,fuel){
+        this.type = type
+        this.model = model
+        this.parts = parts
+        this.parts.quality = parts.engine * parts.power
+        this.fuel = fuel
 
     }
 
+    drive(fuelLose){
+        this.fuel -= fuelLose
+    }
 
-// }
+}
+
 
 let parts = { engine: 6, power: 100 };
 let vehicle = new Vehicle('a', 'b', parts, 200);
